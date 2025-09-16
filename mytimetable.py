287520b29e4761fd38a47972b326bd93e5798b13
lin wu <eddylin2015@gmail.com>
2025-09-16 02:44:37 +0000

    mytimetable

---
+++ mytimetable.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+file_path = Path("mbcholiday.txt")
+content = file_path.read_text(encoding="utf-8")
+holiday=[]
+for elm in content.split("\n"):
+    holiday.append(elm[0:10])
+from datetime import datetime, timedelta
+times_def=["08:30	17:10","08:30	17:15","08:30	17:10","08:30	17:15","08:30	17:10","08:30	12:30", "08:30	16:00"]
+current_date = datetime.now()
+current_date = datetime.strptime("2025-09-01", "%Y-%m-%d")
+for i in range(332):
+    _date = current_date + timedelta(days=i)
+    _date_str=_date.strftime('%Y-%m-%d')
+    _wday=_date.weekday()
+    if _date_str in ["2025-09-01","2025-09-02"] : _wday=6
+    if _date.weekday()==6: continue
+    if _date_str in holiday: continue
+    print(f"{_date.strftime('%Y-%m-%d')}	{times_def[_date.weekday()]}	1")#{_date.weekday()}")
